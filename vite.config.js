bfe4c0457b26bb954f8d00bd0c447f87441afc73
rani <103411066+jinyinlan@users.noreply.github.com>
2023-01-26 21:12:31 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 // vite.config.js
 import vitePluginString from 'vite-plugin-string'
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default {
   plugins: [
     vitePluginString()
   ]
 }
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/noise_sun_system/'
+})
